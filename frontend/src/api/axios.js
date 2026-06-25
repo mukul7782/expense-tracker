bc4045b06f584b1d3009d762d670f8d08bab022c
mukul gupta <mukulgupta91g@gmail.com>
2026-06-25 10:49:07 +0530

    fix api base url

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API = axios.create({ baseURL: " https://expense-tracker-9ksu.onrender.com" });
+const API = axios.create({ baseURL: " https://expense-tracker-9ksu.onrender.com/api" });
 
 API.interceptors.request.use((req) => {
   const token = localStorage.getItem('token');
